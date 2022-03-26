b558fbbbe321fcbdc9b1fbb0088ce45424dcfdcf
OssaiJE <ossaije1@gmail.com>
2022-03-26 20:12:02 +0100

    update JSDSA cheatsheet

--- Searching/searching.js
+++ Searching/searching.js
@@ -13,7 +13,40 @@ function linearSearch(arr, val) {
 linearSearch([34, 51, 1, 2, 3, 45, 56, 100, 687], 100); // 7
 
 /**
- * BINARY SEARCH ALGORITHMS
+ * BINARY SEARCH ALGORITHMS (Has to be sorted, works on integers)
  */
 
-// Big O(n)
+// Big O(LogN)
+
+function binarySearch(arr, elem) {
+  var start = 0;
+  var end = arr.length - 1;
+  var middle = Math.floor((start + end) / 2);
+  while (arr[middle] !== elem && start <= end) {
+    if (elem < arr[middle]) end = middle - 1;
+    else start = middle + 1;
+    middle = Math.floor((start + end) / 2);
+  }
+  return arr[middle] === elem ? middle : -1;
+}
+
+binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 15); // 5
+
+/**
+ * NATIVE STRING SEARCH ALGORITHMS
+ */
+
+// Big O(n^2)
+
+function naiveSearch(long, short) {
+  var count = 0;
+  for (var i = 0; i < long.length; i++) {
+    for (var j = 0; j < short.length; j++) {
+      if (short[j] !== long[i + j]) break;
+      if (j === short.length - 1) count++;
+    }
+  }
+  return count;
+}
+
+naiveSearch("lorie lolly loled", "lol"); // 2
