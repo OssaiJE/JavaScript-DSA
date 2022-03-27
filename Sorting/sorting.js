59f2fe185c2e76036ee8d242b595fae16324bcd6
OssaiJE <ossaije1@gmail.com>
2022-03-27 19:19:46 +0100

    updated JSDSA cheatsheet

--- Sorting/sorting.js
+++ Sorting/sorting.js
@@ -75,3 +75,48 @@ function insertionSort(arr) {
 }
 
 insertionSort([2, 1, 9, 76, 4]); // [1, 2, 4, 9, 76]
+
+/**
+ * FASTER SORTING ALGORITHMS
+ */
+
+/**
+ * MERGE SORT ALGORITHM
+ */
+
+// Big O(nLogn)
+
+// Merge sort helper function
+function merge(arr1, arr2) {
+  let results = [];
+  let i = 0;
+  let j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr2[j] > arr1[i]) {
+      results.push(arr1[i]);
+      i++;
+    } else {
+      results.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    results.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    results.push(arr2[j]);
+    j++;
+  }
+  return results;
+}
+// Recrusive Merge Sort main function
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+}
+
+mergeSort([10, 24, 76, 73]); // [10, 24, 73, 76]
