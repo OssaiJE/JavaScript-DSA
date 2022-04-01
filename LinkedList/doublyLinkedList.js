aca89b4d7b1a45cd2542a448636ac0b1aca07e95
OssaiJE <ossaije1@gmail.com>
2022-04-01 21:35:14 +0100

    updated JSDSA

--- LinkedList/doublyLinkedList.js
+++ LinkedList/doublyLinkedList.js
@@ -122,6 +122,21 @@ class DoublyLinkedList {
     this.length++;
     return true;
   }
+   //   Remove node or list item from a specific index
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+    var removedNode = this.get(index);
+    var beforeNode = removedNode.prev;
+    var afterNode = removedNode.next;
+    beforeNode.next = afterNode;
+    afterNode.prev = beforeNode;
+    removedNode.next = null;
+    removedNode.prev = null;
+    this.length--;
+    return removedNode;
+  }
 }
 
 var list = new DoublyLinkedList();
