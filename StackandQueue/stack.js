d260c069cd4e31dc6a186c6063f19b1602a6ca0e
OssaiJE <ossaije1@gmail.com>
2022-04-04 00:04:55 +0100

    updated JSDSA

--- StackandQueue/stack.js
+++ StackandQueue/stack.js
@@ -11,7 +11,7 @@ class Stack {
     this.last = null;
     this.size = 0;
   }
-//   Add to the top of the stack
+  //   Add to the top of the stack
   push(val) {
     var newNode = new Node(val);
     if (!this.first) {
@@ -24,7 +24,7 @@ class Stack {
     }
     return ++this.size;
   }
-//   Remove from the top of the stack
+  //   Remove from the top of the stack
   pop() {
     if (!this.first) return null;
     var temp = this.first;
@@ -35,4 +35,29 @@ class Stack {
     this.size--;
     return temp.value;
   }
+  //   Convert the stack to array and log to console
+  print() {
+    var arr = [];
+    var current = this.first;
+    while (current) {
+      arr.push(current.value);
+      current = current.next;
+    }
+    console.log(arr);
+  }
 }
+
+var stack = new Stack();
+
+stack.push(100);
+stack.push(201);
+stack.push(250);
+stack.push(350);
+stack.push(999);
+
+// COMMANDS                               OUTPUT
+/**
+ * * $> stack.methodName
+ * $> stack                      Stack {first: Node, last: Node, size: 5}
+ * $> stack.print()                   [999, 350, 250, 201, 100]
+ */
